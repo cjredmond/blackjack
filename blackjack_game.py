3c45edb609d4603c5fd0c100149e86973b9c6101
cjredmond <connor.redmond@gmail.com>
2016-09-24 23:15:07 -0400

    cleaned normal game

--- blackjack_game.py
+++ blackjack_game.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 
 class Card:
@@ -48,10 +49,10 @@ class Player:
         if card.value in tens:
             self.score += 10
         elif card.value == "A":
-            choice = input("Do you want 1 or 11 for the ace? \n >")
-            if choice == 11:
-                self.score += 11
-            else:
+            #choice = input("Do you want 1 or 11 for the ace? \n >")
+            #if choice == "11":
+                #self.score += 11
+            #else:
                 self.score += 1
         else:
             self.score += card.value
@@ -62,7 +63,7 @@ class Player:
 
     def check_bust(self):
         if self.score > 21:
-            print("METHOD BUST")
+            #print("METHOD BUST")
             return True
         else:
             return False
@@ -72,12 +73,15 @@ class Player:
             print("Sorry {} but your already bust".format(self.name))
         elif self.score == 21:
             print("{}, you already have 21".format(self.name))
+            return False
         else:
             decision = input("{}, do you want to (H)it or (S)tand? \n >".format(self.name)).upper()
             if decision == "H":
                 return True
             else:
                 return False
+    def empty_hand(self):
+        self.hand = []
 
 
 connor = Player("Connor")
@@ -86,75 +90,73 @@ dealer = Player("Dealer")
 players = [connor, dealer]
 def initialize_game(players):
     for player in players:
+        player.empty_hand()
+        player.score = 0
         first_card = casino.deal()
         player.add_to_hand(first_card)
         player.count_score(first_card)
         second_card = casino.deal()
         player.add_to_hand(second_card)
         player.count_score(second_card)
-        print(player.show())
         print("\n")
-    print("\n\n")
+    connor.show()
+    print("\n")
 
+def compare_scores(players):
+    print("{}, your score is {}".format(connor.name, connor.score))
+    print("The Dealer's score is {}".format(dealer.score))
 
-
+    if dealer.score == 21:
+        print("The dealer has 21, the Dealer wins")
+    elif connor.score == 21 and dealer.score != 21:
+        print("You've won with 21!")
+    elif connor.score == dealer.score:
+        print("Sorry, the dealer wins a tie")
+    elif connor.score > 21 and dealer.score > 21:
+        print("Both busted")
+    else:
+        if connor.score < 21 and dealer.score < 21:
+            if connor.score > dealer.score:
+                print("You beat the dealer")
+            else:
+                print("The dealer has a better score")
 
 
 
 def playing_backjack(players):
+    casino = Deck()
     initialize_game(players)
-    for player in players:
-        print(player.show_score())
+    print(connor.show_score())
     print("\n")
-
-    while players[0].check_bust() == False or players[1].check_bust() == False:
-                if players[0].check_bust() == False:
-                    print(players[0].show_score())
-
-                    choice = players[0].hit_or_stand()
-                    if choice == True:
-                        new_card = casino.deal()
-                        new_card.show()
-                        players[0].add_to_hand(new_card)
-                        players[0].count_score(new_card)
-                        print(players[0].show_score())
-                    
-
-
-                else:
-                    pass
-
-
-
-
-
-                print(players[1].show_score())
-                if players[1].check_bust() == False:
-                    choice = players[1].hit_or_stand()
-                    if choice == True:
-                        new_card = casino.deal()
-                        new_card.show()
-                        players[1].add_to_hand(new_card)
-                        players[1].count_score(new_card)
-                        print(players[1].show_score())
-                else:
-                    pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    while connor.check_bust() == False:
+        choice = connor.hit_or_stand()
+        if choice == True:
+            new_card = casino.deal()
+            new_card.show()
+            connor.add_to_hand(new_card)
+            connor.count_score(new_card)
+            print(connor.show_score())
+        else:
+            print("You chose to stand")
+            break
+    while dealer.check_bust() == False:
+        choice = True
+        if connor.score <= dealer.score:
+            choice = False
+        elif connor.score > 21:
+            choice = False
+        if choice == True:
+            print("The dealer chose to hit")
+            new_card = casino.deal()
+            new_card.show()
+            dealer.add_to_hand(new_card)
+            dealer.count_score(new_card)
+        else:
+            break
+    dealer.show()
+    compare_scores(players)
+    play_again = input("Do you want to play again? Y/n:   ").lower()
+    if play_again == "y":
+        playing_backjack(players)
 
 playing_backjack(players)
